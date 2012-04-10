77a9db1ad87553d383a741acf0e9809ead33686e
Kori Roys <kori@koriroys.com>
2012-04-10 14:05:08 -0500

    As the tests are written, I can just push the array on as the last element, then pop it off and reverse it to get the tests to pass. I don't like that. I think the pop and push of the array should pop/push different lengths. Below is my passing submission. Thoughts?
    class Stack
      attr_accessor :stack
      
      def initialize(input)
        self.stack = input
      end
    
      def pop(*args)
        return stack.pop if args.empty?
        stack.pop.reverse # This shouldn't work.
      end
    
      def push(args)
        stack.push(args)
        true
      end
    
      def to_a
        stack
      end
    end

--- db/seeds.rb
+++ db/seeds.rb
@@ -192,8 +192,8 @@ stack = Stack.new([5, 6, 7, 8])
 assert_equal stack.pop, 8
 assert_equal stack.pop, 7
 assert_equal stack.push([4, 2]), true
-assert_equal stack.pop(2), [2, 4]
-assert_equal stack.to_a, [5, 6]
+assert_equal stack.pop(3), [2, 4, 6]
+assert_equal stack.to_a, [5]
 code_block
                )
 
